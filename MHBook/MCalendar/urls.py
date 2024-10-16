9dde027f9b178a1e4d4a7503d064793d770521f0
DragonizerX <103155360+DragonizerX@users.noreply.github.com>
2024-10-16 03:42:31 +0100

    Updated Initial Commit

---
+++ MHBook/MCalendar/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = "MCalendar"
+
+urlpatterns = [
+
+    # Accessable via http://127.0.0.1:8000/MCalendar/example/
+    path("example/", views.examplePage, name="example"),
+    
+]
--- MHBook/MCalendar/views.py
+++ MHBook/MCalendar/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
 
-
+def examplePage(request):
+    return render(request, 'example.html')
