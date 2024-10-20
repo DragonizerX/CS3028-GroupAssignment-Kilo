0a527a1665459a576d2a7dab15d78761ece46b9e
Oliwier Kaminski <u02ok23@abdn.ac.uk>
2024-10-20 17:49:41 +0100

    Edit Booking working !!!1!

--- MHBook/MCalendar/views.py
+++ MHBook/MCalendar/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.http import HttpResponse
 from django.template import loader
@@ -18,9 +18,57 @@ def myBookings(request):
     return HttpResponse(template.render(context, request))
 
 def editBooking(request, id):
-    editBooking = Bookings.objects.filter(id=id).values()
+    booking = get_object_or_404(Bookings, id=id)
+    
+    if request.method == 'POST':
+        fullname = request.POST.get('fullname')
+        if fullname:
+            booking.fullname = fullname
+
+        phone = request.POST.get('phone')
+        if phone:
+            booking.phone = int(phone)
+
+        email = request.POST.get('email')
+        if email:
+            booking.email = email
+
+        supervisor = request.POST.get('supervisor')
+        if supervisor:
+            booking.supervisor = supervisor
+
+        organisation = request.POST.get('organisation')
+        if organisation:
+            booking.organisation = organisation
+
+        date = request.POST.get('date')
+        if date:
+            booking.date = date
+
+        start = request.POST.get('start')
+        if start:
+            booking.start = start
+
+        finish = request.POST.get('finish')
+        if finish:
+            booking.finish = finish
+
+        room = request.POST.get('room')
+        if room:
+            booking.room = room
+
+        equipment = request.POST.get('equipment')
+        if equipment:
+            booking.equipment = equipment
+
+        equipmentid = request.POST.get('equipmentid')
+        if equipmentid:
+            booking.equipmentid = equipmentid
+
+        booking.save()
+
     template = loader.get_template('editBooking.html')
     context = {
-        'editBooking': editBooking,
+        'editBooking': [booking],
     }
     return HttpResponse(template.render(context, request))
