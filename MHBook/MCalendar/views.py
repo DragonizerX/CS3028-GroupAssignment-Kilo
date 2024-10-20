4c1c30e3dc525812480fb11610392890a97a5e28
Oliwier Kaminski <u02ok23@abdn.ac.uk>
2024-10-20 18:13:33 +0100

    small change

--- MHBook/MCalendar/views.py
+++ MHBook/MCalendar/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic
 from django.http import HttpResponse
 from django.template import loader
 from .models import Bookings
