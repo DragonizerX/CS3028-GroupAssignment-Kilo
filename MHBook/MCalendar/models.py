d0d50d29615d170cd6b5b469f3797070d4cdc6d1
DragonizerX <103155360+DragonizerX@users.noreply.github.com>
2024-11-03 00:12:19 +0000

    Merging Fixes for Staging

--- MHBook/MCalendar/models.py
+++ MHBook/MCalendar/models.py
@@ -65,32 +65,6 @@ class Supervisor(models.Model):
     password = models.CharField(max_length=128, null=True)  # Initially allow null
     telephone = models.CharField(max_length=15, blank=True)
 
-    
-
-""" - ahahahah
-class Bookings(models.Model):
-    fullname = models.CharField(max_length=64, blank=False, null=False)
-    phone = models.IntegerField(blank=False, null=False)
-    email = models.EmailField(blank=False, null=False)
-    supervisor = models.CharField(max_length=64, default='N/A')
-    organisation = models.CharField(max_length=128, default='N/A')
-    date = models.DateField(blank=False, null=False)
-    start = models.TimeField(blank=False, null=False)
-    finish = models.TimeField(blank=False, null=False)
-    room = models.CharField(max_length=16, blank=False, null=False)
-    equipment = models.CharField(max_length=64, blank=False, null=False)
-    equipmentid = models.CharField(max_length=32, default='N/A')
-"""
-
-""" --- Redundant, delete later
-class AccountRequest(models.Model):
-    fullname = models.CharField(max_length=64, blank=False, null=False)
-    phone = models.IntegerField(blank=False, null=False)
-    email = models.EmailField(blank=False, null=False)
-    supervisor = models.CharField(max_length=64, default='N/A')
-    organisation = models.CharField(max_length=128, blank=False, null=False)
-    isAccepted = models.BooleanField(default=False)
-"""
 
 class Event(models.Model):
     bookingName = models.CharField(max_length=80)
