ed3e9c5a8fef6f72bc8a42abe0827a51d2540a16
Adam-Ballantyne <u18ab22@abdn.ac.uk>
2024-11-11 11:24:39 +0000

    Initial Setup for Histology App

--- MHBook/mysite/settings.py
+++ MHBook/mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'MCalendar'
+    'MCalendar',
+    'HSamples'
 ]
 
 AUTH_USER_MODEL = 'MCalendar.Users'
--- MHBook/mysite/urls.py
+++ MHBook/mysite/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("MCalendar/", include('MCalendar.urls')),
+    path("HSamples/", include('HSamples.urls')),
     path('admin/', admin.site.urls),
 ]
